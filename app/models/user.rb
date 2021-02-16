770421abf924a9f29143ea59153d145aa795462f
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-02-17 05:14:19 +0900

    氏名のバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
+  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' } do
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
-  validates :birthday, presence: true
+  end
 end
