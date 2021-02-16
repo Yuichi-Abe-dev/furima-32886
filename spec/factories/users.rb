236eb6e22826c3449735e275f88efeb5e461bfd5
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-02-17 01:59:38 +0900

    FactoryBotの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,14 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
     last_name {Gimei.name.last.kanji}
     first_name {Gimei.name.first.kanji}
     last_name_kana {Gimei.name.last.katakana}
     first_name_kana {Gimei.name.last.hiragana}
-    birthday {Faker::Date.between(from: '1990-01-01', to: '2021-01-01')}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    birthday {Faker::Date.between(from: '1980-01-01', to: '2020-01-01')}
+
   end
 end
