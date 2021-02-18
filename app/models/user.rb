834ce7d3bd778f85cc8a2935da3a6dbed0d0e856
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-02-18 23:07:26 +0900

    パスワードのバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :birthday, presence: true
+  with_options presence: true, format: { with: /\A[a-zA-Z0-9]+\z/, message: '全角文字を使用してください' } do
+    validates :password
+  end
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
