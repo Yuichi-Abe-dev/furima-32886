a4a801c84c8d42ce5443ac776e8c1c2f65dca68a
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-02-19 22:40:33 +0900

    マイグレーションファイルにカラムを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  #Purchaseテーブル作成後にコメントアウトを外す
+  #has_one :purchase
+  has_one_attached :image
+  validates :name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_date_id, :price, presence: true
 end
