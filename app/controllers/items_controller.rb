8e7f55a1168c54ea606732c3f0aad3c4f548c0d1
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-02-25 00:13:43 +0900

    createアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,13 @@ class ItemsController < ApplicationController
     @items = Item.new
   end
 
+  def create
+    @item = Item.create(item_params)
+  end
+
+private
+  def item_params
+    params.require(:item).permit(:name, :discription, :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_date_id).merge(user_id: current_user.id)
+  end
+
 end
