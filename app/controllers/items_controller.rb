ffe07149a80a1758fad154413399fb28d172af8a
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-02-25 00:18:27 +0900

    itemコントローラーにindexアクションの仮置と非ログインユーザーの処理の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,9 @@
 class ItemsController < ApplicationController
-  #before_action :authenticate_user!, except:
+before_action :authenticate_user!, except: [:index]
+before_action :move_to_index, only: :new
+
+  def index
+  end
 
   def new
     @items = Item.new
@@ -13,5 +17,10 @@ private
   def item_params
     params.require(:item).permit(:name, :discription, :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_date_id).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in?
+      redirect_to action: :index
 
 end
