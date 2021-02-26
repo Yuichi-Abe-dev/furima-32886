8b7ba955e4ac81bb0b741879734afc6c7575d98c
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-02-26 23:27:16 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,7 @@ before_action :move_to_loginpage, only: :new
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
   end
 
   def create
@@ -20,7 +20,7 @@ before_action :move_to_loginpage, only: :new
 
 private
   def item_params
-    params.require(:item).permit(:name, :discription, :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_date_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
 
   def move_to_loginpage
