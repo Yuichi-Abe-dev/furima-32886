5b1b8ba6f2a5fd4c616519cac28b8a8d23bbafb0
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-03-03 23:06:15 +0900

    結合テストコードの雛形の作成

--- spec/system/items_spec.rb
+++ spec/system/items_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Items", type: :system do
+RSpec.describe "商品出品", type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context '出品画面への遷移' do
+    it 'ログインユーザーは商品投稿ページへ遷移できる' do
+      # トップページに遷移する
+      # 商品出品ページへ遷移する
+    end
+    it 'ログインユーザーでないと投稿画面へ遷移ができない' do
+      # トップページに遷移する
+      # 商品出品ページへ遷移を試みる
+      # ログインページへ遷移する
+    end
+  end
 end
