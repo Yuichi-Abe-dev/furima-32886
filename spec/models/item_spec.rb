e8614147052fdacae8a33b8d6e8fec227d716eae
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-03-03 20:42:34 +0900

    商品出品機能のモデルの単体テストコードの雛形作成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+    context '商品の保存がうまくいくとき' do
+      it '全てのカラムの値が存在していれば保存できること' do
+
+      end
+
+    end
+    context '商品の保存がうまくいかないとき' do
+      it 'imageが空では保存できないこと' do
+
+      end
+
+      it 'nameが空では保存できないこと' do
+
+      end
+
+      it 'descriptionが空では保存できないこと' do
+
+      end
+
+      it 'category_idが空では保存できないこと' do
+
+      end
+
+      it 'condition_idが空では保存できないこと' do
+
+      end
+
+      it 'postage_idが空では保存できないこと' do
+
+      end
+
+      it 'prefecture_idが空では保存できないこと' do
+
+      end
+
+      it 'shipping_date_idが空では保存できないこと' do
+
+      end
+
+      it 'が空では保存できないこと' do
+
+      end
+
+      it 'priceが空では保存できないこと' do
+
+      end
+    end
+  end
 end
