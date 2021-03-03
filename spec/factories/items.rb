33bc74e9320b84ca346e8b137bbd4b728bf2ff56
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-03-03 22:04:27 +0900

    itemのモデルの単体テストコードの作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,16 +1,16 @@
 FactoryBot.define do
   factory :item do
-    name{Faker::String.random}
-    description{Faker::Quote.yoda}
-    category_id{Faker::Number.between(from: 2, to: 11)}
-    condition_id{Faker::Number.between(from: 2, to: 7)}
-    postage_id{Faker::Number.between(from: 2, to: 3)}
-    prefecture_id{Faker::Number.between(from: 2, to: 48)}
-    shipping_date_id{Faker::Number.between(from: 2, to: 4)}
+    name {Faker::Beer.name}
+    description {Faker::Quote.yoda}
+    category_id {Faker::Number.between(from: 2, to: 11)}
+    condition_id {Faker::Number.between(from: 2, to: 7)}
+    postage_id {Faker::Number.between(from: 2, to: 3)}
+    prefecture_id {Faker::Number.between(from: 2, to: 48)}
+    shipping_date_id {Faker::Number.between(from: 2, to: 4)}
     price{Faker::Number.between(from: 300, to: 9999999)}
     association :user
-    after(:build) do |prototype|
-      prototype.image.attach(io: File.open('public/images/400x400_01.png'), filename: '400x400_01.png')
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/400x400_01.png'), filename: '400x400_01.png')
     end
   end
 end
