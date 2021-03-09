cdba78a8b79ad925ee183efcbb916492999c580b
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-03-09 15:41:05 +0900

    Rubocopの実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,8 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_date
   belongs_to :user
-  #商品購入機能実装時にコメントアウトを外す
-  #has_one :purchase
+  # 商品購入機能実装時にコメントアウトを外す
+  # has_one :purchase
   has_one_attached :image
   with_options presence: true do
     validates :image, :name, :description
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,8 +1,7 @@
-=begin 商品購入機能実装時にコメントアウトを外す
-class Purchase < ApplicationRecord
-  belongs_to :user
-  belongs_to :item
-  # Addressテーブル作成後にコメントアウトを外す
-  # has_one :address
-end
-=end
+# 商品購入機能実装時にコメントアウトを外す
+# class Purchase < ApplicationRecord
+#   belongs_to :user
+#   belongs_to :item
+#   # Addressテーブル作成後にコメントアウトを外す
+#   # has_one :address
+# end
