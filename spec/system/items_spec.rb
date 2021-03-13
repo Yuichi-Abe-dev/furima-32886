f4b9c82cca6e13939c5abd0acb30f9bc74205818
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-03-13 21:31:30 +0900

    Rubocopの実行

--- spec/system/items_spec.rb
+++ spec/system/items_spec.rb
@@ -435,17 +435,17 @@ RSpec.describe '商品情報削除', type: :system do
       # 削除ボタンの存在を確認
       expect(page).to have_content('削除')
       # 削除ボタンをクリックするとItemモデルのカウントが減ること確認
-      expect{
+      expect do
         click_on('削除')
-      }.to change { Item.count }.by(-1)
+      end.to change { Item.count }.by(-1)
       # 削除後にトップページに遷移したことを確認する
       expect(current_path).to eq(root_path)
       # トップページに遷移する
       visit root_path
       # トップページには先ほど削除した内容の商品が存在しないことを確認する
-      expect(page).to have_no_content("#{@item.name}")
-      expect(page).to have_no_content("#{@item.price}")
-      expect(page).to have_no_content("#{@item.postage.name}")
+      expect(page).to have_no_content(@item.name.to_s)
+      expect(page).to have_no_content(@item.price.to_s)
+      expect(page).to have_no_content(@item.postage.name.to_s)
     end
   end
 end
