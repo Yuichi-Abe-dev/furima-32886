fbb8b3869ee1a7f42ff4142c5ab0b3758aaaab1a
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-03-13 21:30:44 +0900

    不要なコメントアウトの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-  #before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_toppage, only: [:edit, :update, :destroy]
