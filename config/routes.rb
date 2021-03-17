0ee15303e7204f69663c1d2a82b7c03ce98a6132
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-03-18 01:23:30 +0900

    purchaseのルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items
-  resources :purchases, only: [:index, :new, :create]
+  resources :purchases, only: [:index, :create]
 end
