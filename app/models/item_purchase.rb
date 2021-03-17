f2715b50a1463210d4936626866d84a8f34cf3fb
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-03-18 00:18:17 +0900

    item_purchaseモデルにカラム名を追記

--- app/models/item_purchase.rb
+++ app/models/item_purchase.rb
@@ -1,3 +1,4 @@
 class ItemPurchase
   include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipalities, :address_line1, :address_line2, :phone_number, :purchase_id
 end
