2ae39df9a8c0b28998694b54e20bd4eda8d07e3d
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-03-18 01:22:37 +0900

    purchaseコントローラに商品購入用のアクションを定義

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,21 @@
 class PurchasesController < ApplicationController
   def index
+    @item_purchase = ItemPurchase.new
+  end
+ 
+  def create
+    @item_purchase = ItemPurchase.new(purchase_params)
+     if @item_purchase.valid?
+       @item_purchase.save
+       redirect_to root_path
+     else
+       render action: :index
+     end
+  end
+ 
+  private
+   # 全てのストロングパラメーターを1つに統合
+  def purchase_params
+   params.require(:item_purchase).permit(:user_id, :item_id, :postal_code, :prefecture_id, :municipalities, :address_line1, :address_line2, :phone_number, :purchase_id)
   end
 end
