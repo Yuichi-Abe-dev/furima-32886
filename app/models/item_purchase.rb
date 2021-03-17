8c79b52c54c159e4f73f09efcc01089e9e016f7e
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-03-18 00:46:23 +0900

    item_purchaseモデルにaddressesテーブル用のバリデーションを追記

--- app/models/item_purchase.rb
+++ app/models/item_purchase.rb
@@ -1,4 +1,11 @@
 class ItemPurchase
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipalities, :address_line1, :address_line2, :phone_number, :purchase_id
+
+  with_options presence: true do
+    validates :municipalities, :address_line1
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :phone_number, format: { with: /\A\d{10}$|^\d{11}\z/ }
+  end
 end
