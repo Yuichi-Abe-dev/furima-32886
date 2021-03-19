032b561ae0f4cbf3bb4b5182203a9c15e80034fd
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-03-19 22:07:09 +0900

    purchaseをitemのルーティングのネスト

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,7 @@
 class PurchasesController < ApplicationController
   def index
     @item_purchase = ItemPurchase.new
-    #@item = Item.find(params[:item_id])
+    @item = Item.find(params[:item_id])
   end
  
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'purchases/index'
   devise_for :users
   root to: "items#index"
-  resources :items
-  resources :purchases, only: [:index, :create]
+  resources :items do
+    resources :purchases, only: [:index, :create]
+  end
 end
