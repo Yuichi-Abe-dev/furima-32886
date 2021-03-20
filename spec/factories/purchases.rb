632c3d69495d1c8d3560e07e26b08aece590f3e6
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-03-21 03:09:36 +0900

    FactoryBotの定義

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -1,4 +1,11 @@
 FactoryBot.define do
   factory :purchase do
+    token {"tok_abcdefghijk00000000000000000"}
+    postal_code { "000-0000" }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    municipalities {Gimei.city.kanji}
+    address_line1 {"#{Gimei.town.kanji }"+"1-2-3"}
+    address_line2 { Faker::Address.secondary_address }
+    phone_number { Faker::Number.leading_zero_number(digits: 11) }
   end
 end
