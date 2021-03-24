980e12fd6e40f7a452f0b991cbaeb1f1103d217d
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-03-24 23:43:51 +0900

    商品売切れ時の処理をitems_controllerのedit,updateアクションに追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,7 +50,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_toppage
-    redirect_to root_path unless current_user.id == @item.user_id
+    redirect_to root_path if current_user.id != @item.user_id || @item.purchase != nil
   end
 
   def set_item
